76e3b08be41bdde902941ae50144c879a83c5701
doitkys <doitkysoo@gmail.com>
2025-09-18 04:22:43 +0900

    html css sidemenu

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const API_KEY = `4a54a306c22d41b3a3845502b5c0aedc`;
 let news = [];
+
 const getLatestNews = async () => {
   const url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&apiKey=${API_KEY}`
@@ -11,4 +12,19 @@ const getLatestNews = async () => {
   console.log('ddd', news);
 };
 
-getLatestNews();
+let searchIcon = () => {
+  let searchArea = document.getElementById('search-area');
+
+  if (searchArea.style.display === 'inline') {
+    searchArea.style.display = 'none';
+  } else {
+    searchArea.style.display = 'inline';
+  }
+};
+
+let openSideButton = () => {
+  document.getElementById('side-nav').style.width = '250px';
+};
+let closeSideButton = () => {
+  document.getElementById('side-nav').style.width = '0';
+};
